e1c5a47bae0bb77983a765c7f804e110a06f1754
taidashi <b.grossa1014@gmail.com>
2026-05-29 00:59:43 +0900

    Create todo app with complete and delete features

---
+++ src/main/java/com/example/todoapp/Task.java
@@ -0,0 +1,24 @@
+package com.example.todoapp;
+
+public class Task {
+	
+	private String name;
+	private boolean completed;
+	
+	public Task(String name) {
+		this.name = name;
+		this.completed = false;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public boolean isCompleted() {
+		return completed;
+	}
+	
+	public void setCompleted(boolean completed) {
+		this.completed = completed;
+	}
+}
---
+++ src/main/java/com/example/todoapp/TodoController.java
@@ -0,0 +1,51 @@
+package com.example.todoapp;
+
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class TodoController {
+	
+	ArrayList<Task> tasks = new ArrayList<>();
+
+	@GetMapping("/")
+	public String index(Model model) {
+		
+		model.addAttribute("tasks", tasks);
+		
+		return "index";
+	}
+	
+	@PostMapping("/add")
+	public String addTask(
+			@RequestParam String taskName
+		) {
+			
+			tasks.add(new Task(taskName));
+			
+			return "redirect:/";
+	}
+	
+	@PostMapping("/delete")
+	public String deleteTask(
+		@RequestParam int index
+	) {
+		
+		tasks.remove(index);
+		
+		return "redirect:/";
+	}
+	
+	@PostMapping("/complete")
+	public String completeTask(@RequestParam int index) {
+		
+		tasks.get(index).setCompleted(true);
+		
+		return "redirect:/";
+	}
+}
